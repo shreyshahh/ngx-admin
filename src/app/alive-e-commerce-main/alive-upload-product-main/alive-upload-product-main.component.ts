933de5c175603f813998a19940a6ce2e3c55ce2b
Shrey-shah <shreyshahh@users.noreply.github.com>
2020-06-29 12:44:09 +0530

    3 stepper added

--- src/app/alive-e-commerce-main/alive-upload-product-main/alive-upload-product-main.component.ts
+++ src/app/alive-e-commerce-main/alive-upload-product-main/alive-upload-product-main.component.ts
@@ -12,30 +12,46 @@ export class AliveUploadProductMainComponent implements OnInit {
 
   options: string[];
   filteredOptions$: Observable<string[]>;
+  imagesCounter: number;
+  imagesText: number;
+  imagesCounterArray = [];
   @ViewChild('autoInput') input;
   constructor() { }
 
   ngOnInit(): void {
+    this.imagesCounter = 0;
     this.options = ['Option 1', 'Option 2', 'Option 3'];
     this.filteredOptions$ = of(this.options);
 
-}
-private filter(value: string): string[] {
-  const filterValue = value.toLowerCase();
-  return this.options.filter(optionValue => optionValue.toLowerCase().includes(filterValue));
-}
+  }
+  private filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter(optionValue => optionValue.toLowerCase().includes(filterValue));
+  }
 
-getFilteredOptions(value: string): Observable<string[]> {
-  return of(value).pipe(
-    map(filterString => this.filter(filterString)),
-  );
-}
+  getFilteredOptions(value: string): Observable<string[]> {
+    return of(value).pipe(
+      map(filterString => this.filter(filterString)),
+    );
+  }
 
-onChange() {
-  this.filteredOptions$ = this.getFilteredOptions(this.input.nativeElement.value);
-}
+  onChange() {
+    this.filteredOptions$ = this.getFilteredOptions(this.input.nativeElement.value);
+  }
 
-onSelectionChange($event) {
-  this.filteredOptions$ = this.getFilteredOptions($event);
-}
+  onSelectionChange($event) {
+    this.filteredOptions$ = this.getFilteredOptions($event);
+  }
+  imagesDisplay(imagesText) {
+
+    if (this.imagesCounterArray.length > 0) {
+      this.imagesCounterArray.splice(0, this.imagesCounterArray.length);
+    }
+    this.imagesCounter = imagesText;
+
+    for (let i = 0; i < this.imagesCounter; i++) {
+      this.imagesCounterArray.push(i);
+    }
+
+  }
 }
